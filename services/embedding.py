a8361a678476b0e1abb26723966b1e8b34041c1f
@miguel.barrios <miguel.barrios@intelygenz.com>
2025-05-18 20:31:16 +0200

    feat: Implement embedding service and corresponding tests

---
+++ services/embedding.py
@@ -0,0 +1,13 @@
+from langchain.embeddings import OllamaEmbeddings
+from typing import List
+
+class EmbeddingService:
+    def __init__(self, model: str = "nomic-embed-text"):
+        self.model_name = model
+        self.client = OllamaEmbeddings(model=self.model_name)
+
+    def embed_documents(self, texts: List[str]) -> List[List[float]]:
+        return self.client.embed_documents(texts)
+
+    def embed_query(self, query: str) -> List[float]:
+        return self.client.embed_query(query)
