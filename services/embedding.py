e2498f5dff10d0c1e597e601b72cfb8d1bf7d61f
@miguel.barrios <miguel.barrios@intelygenz.com>
2025-05-27 10:42:52 +0200

    feat: update dependencies and switch embedding implementation

--- services/embedding.py
+++ services/embedding.py
@@ -1,12 +1,11 @@
 from typing import List
 
-from langchain_community.embeddings import OllamaEmbeddings
+from langchain_openai import OpenAIEmbeddings
 
 
 class EmbeddingService:
-    def __init__(self, model: str = "nomic-embed-text"):
-        self.model_name = model
-        self.client = OllamaEmbeddings(model=self.model_name)
+    def __init__(self):
+        self.client = OpenAIEmbeddings(model="text-embedding-3-small")
 
     def embed_documents(self, texts: List[str]) -> List[List[float]]:
         return self.client.embed_documents(texts)
