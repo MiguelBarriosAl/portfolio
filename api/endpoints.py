76ae7d2e8224b3ad695879156f89c1a8097c5ee6
@miguel.barrios <miguel.barrios@intelygenz.com>
2025-05-29 00:38:16 +0200

    Refactor project dependencies and remove query engine module
    - Updated dependencies in pyproject.toml, including version changes for openai, langchain, and langchain-community.
    - Added package inclusion for ai_portfolio.
    - Removed the query_engine.py module, which contained the logic for query processing using Langchain and Ollama.

--- api/endpoints.py
+++ api/endpoints.py
@@ -1,10 +1,10 @@
 import shutil
 from pathlib import Path
 
-from fastapi import APIRouter, File, Header, HTTPException, UploadFile, status
+from fastapi import APIRouter, File, Header, HTTPException, UploadFile
 from langchain.chains import RetrievalQA
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores.qdrant import Qdrant
+from langchain_community.vectorstores import Qdrant
 from langchain_openai import ChatOpenAI
 
 from config import settings
@@ -19,7 +19,6 @@ router = APIRouter()
 @router.post("/ask")
 def ask_profile(input: AskRequest):
     vectorstore = get_vectorstore()
-
     retriever = Qdrant(
         client=vectorstore.client,
         collection_name=vectorstore.collection_name,
@@ -45,39 +44,30 @@ def ask_profile(input: AskRequest):
     return {
         "query": input.query,
         "response": result["result"],
-        # "sources": [doc.metadata.get("source") for doc in result["source_documents"]]
     }
 
 
 @router.post("/upload-file")
 async def upload_file(file: UploadFile = File(...), x_api_key: str = Header(None)):
     if x_api_key != settings.upload_api_key:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid or missing API Key",
-        )
+        raise HTTPException(status_code=401, detail="Invalid or missing API Key")
 
     if not (file.filename.endswith(".pdf") or file.filename.endswith(".txt")):
         raise HTTPException(
             status_code=400, detail="Only .pdf or .txt files are supported."
         )
 
-    # 1. Guardar temporalmente
     save_path = Path(f"data/dev/{file.filename}")
     with save_path.open("wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
-    # 2. Cargar documentos (mismo método para ambos tipos)
     documents = load_documents(str(save_path))
-
-    # 3. Chunking
     splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
     split_docs = splitter.split_documents(documents)
 
     texts = [doc.page_content for doc in split_docs]
     metadatas = [doc.metadata for doc in split_docs]
 
-    # 4. Indexar en Qdrant
     store = get_vectorstore()
     store.index_documents(texts, metadatas)
 
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from api.endpoints import router as ask_router
+from api.endpoints import router
 
 app = FastAPI()
-app.include_router(ask_router)
+app.include_router(router)
